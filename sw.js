2f7e2dae95098a9e746f32235a3b09922586c5a7
Josiah-J-Projects <Josiah.jukes@gmail.com>
2026-05-14 18:07:34 -0600

    Updates

--- sw.js
+++ sw.js
@@ -2,9 +2,6 @@ const CACHE = 'cashlytics_v0.8.0'
 
 //install the app
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(['/']))
-  )
   self.skipWaiting()
 })
 
@@ -20,25 +17,23 @@ self.addEventListener('activate', e => {
   self.clients.claim()
 })
 
-//Check cache when app requests a file, otherwise fetch from network
+//Check cache when app requests a file
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return
-  //only cache files from origin
-  if (!e.request.url.startsWith(self.location.origin)) return
 
   e.respondWith(
-    caches.open(CACHE).then(async cache => {
-      //check if this file is already cached
-      const cached = await cache.match(e.request)
-      // otherwise download newest version from internet
-      const networkPromise = fetch(e.request)
-        .then(response => {
-           //only cache successful responses
-          if (response.ok) cache.put(e.request, response.clone())
-          return response
-        })
-        .catch(() => null)
-      return cached || await networkPromise
-    })
+    fetch(e.request)
+      .then(response => {
+        //cache successful responses as we go
+        if (response && response.status === 200) {
+          const clone = response.clone()
+          caches.open(CACHE).then(cache => cache.put(e.request, clone))
+        }
+        return response
+      })
+      .catch(() => {
+        //if network failed, try the cache (offline support)
+        return caches.match(e.request)
+      })
   )
 })
